9cd57279c83712f52e20176942dd95a6ec2d47b6
Resma Adi Nugroho <resmaadi@students.undip.ac.id>
2023-09-21 17:38:46 +0700

    first commit

---
+++ services/crud.php
@@ -0,0 +1,5 @@
+<?php
+class CRUD {
+    $db_host='localhost';
+}
+?>
---
+++ services/db.php
@@ -0,0 +1,18 @@
+<?php
+$db_host='localhost';
+$db_database='cukurukuk';
+$db_username='root';
+$db_password='';
+
+$db = new mysqli($db_host, $db_username, $db_password, $db_database);
+if($db->connect_errno){
+    die("Gagal dalam koneksi ke database: </br> ".$db->connect_error);
+}
+
+function test_input($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
