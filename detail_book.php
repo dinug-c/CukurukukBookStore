8f0c4940198fefe6bbbe762de2bb363d1b690e4e
Resma Adi Nugroho <resmaadi@students.undip.ac.id>
2023-09-24 23:46:38 +0700

    fix: detail book tampilan

--- detail_book.php
+++ detail_book.php
@@ -13,39 +13,51 @@ headerComponentBootstrap("Detail ".$nama." - Cukurukuk BookStore");
 <div class="container mt-5">
     <div class="row">
         <div class="col-md-6">
-            <h1>Detail Buku</h1>
-            <p>Judul Buku: <?php echo $nama ?></p>
-            <p>ISBN: <?php echo $result['ISBN'] ?></p>
-            <p>Kategori: <?php echo $result['Category'] ?></p>
-            <p>Penulis: <?php echo $result['Author'] ?></p>
-            <p>Harga: <?php echo $result['Price'] ?></p>
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="card-title">Detail Buku</h1>
+                    <p class="card-text">Judul Buku: <?php echo $nama ?></p>
+                    <p class="card-text">ISBN: <?php echo $result['ISBN'] ?></p>
+                    <p class="card-text">Kategori: <?php echo $result['Category'] ?></p>
+                    <p class="card-text">Penulis: <?php echo $result['Author'] ?></p>
+                    <p class="card-text">Harga: <?php echo $result['Price'] ?></p>
+                </div>
+            </div>
         </div>
         <div class="col-md-6">
-            <h2>Review Buku</h2>
-            <?php
-            $res = getSingleByQuery("reviews", "ISBN", $id);
-            echo '<p>Review</p>';
-            $i = 1;
-            while ($row = $res->fetch_object()) {
-                echo '<p>'.$i.'. '. $row->review . '</p>';
-                echo '<hr>';
-                $i++;
-            }
-            ?>
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">Review Buku</h2>
+                    <?php
+                    $res = getSingleByQuery("reviews", "ISBN", $id);
+                    echo '<p class="card-text">Review</p>';
+                    $i = 1;
+                    while ($row = $res->fetch_object()) {
+                        echo '<p class="card-text">'.$i.'. '. $row->review . '</p>';
+                        echo '<hr>';
+                        $i++;
+                    }
+                    ?>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 
 <div class="container mt-5">
-    <h2>Tambahkan Review</h2>
-    <form method="post" action="add_review.php">
-        <input type="hidden" name="isbn" value="<?php echo $id; ?>">
-        <div class="mb-3">
-            <label for="review" class="form-label">Review</label>
-            <textarea class="form-control" id="review" name="review" rows="4" required></textarea>
+    <div class="card">
+        <div class="card-body">
+            <h2 class="card-title">Tambahkan Review</h2>
+            <form method="post" action="add_review.php">
+                <input type="hidden" name="isbn" value="<?php echo $id; ?>">
+                <div class="mb-3">
+                    <label for="review" class="form-label">Review</label>
+                    <textarea class="form-control" id="review" name="review" rows="4" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Tambah Review</button>
+            </form>
         </div>
-        <button type="submit" class="btn btn-primary">Tambah Review</button>
-    </form>
+    </div>
 </div>
 
 <?php
